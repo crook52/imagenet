809477d5bf298d8fd3e6cefba22e65f137d4b50d
crook52 <kazuking52@gmail.com>
2017-12-07 18:28:25 +0900

    test

---
+++ 1/commander.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import subprocess as sp
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--rootdir','--r',default='~/chainer-master')
+parser.add_argument('--o',default='result')
+parser.add_argument('--cuDNN',default=False)
+sp.check_output('python3 train_imagenet_data_parallel.py \
+train.txt val.txt -a resnet50 -B 64 -j 8 -m train_mean.npy -R ~/workspace/ -o 1205 -E 1')
+
+
+
+aaaaaaaa
