5b81643958840f688bf9d936926d15ea2d66b822
crook52 <kazuking52@gmail.com>
2017-12-07 18:44:57 +0900

    test

--- 1/commander.py
+++ 1/commander.py
@@ -10,5 +10,3 @@ parser.add_argument('--o',default='result')
 parser.add_argument('--cuDNN',default=False)
 sp.check_output('python3 train_imagenet_data_parallel.py \
 train.txt val.txt -a resnet50 -B 64 -j 8 -m train_mean.npy -R ~/workspace/ -o 1205 -E 1')
-
-test
