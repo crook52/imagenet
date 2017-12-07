b90fc2c5cb3bf3896aa7c2c4f85cec34898cf9ba
crook52 <kazuking52@gmail.com>
2017-12-07 18:49:40 +0900

    test

--- 1/commander.py
+++ 1/commander.py
@@ -8,5 +8,15 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--rootdir','--r',default='~/chainer-master')
 parser.add_argument('--o',default='result')
 parser.add_argument('--cuDNN',default=False)
+
+args = parser.parse_args()
+
+if args.cuDNN:
+    sp.check_output("export CHAINER_CUDNN=1")
+else:
+    sp.check_output("export CHAINER_CUDNN=0")
+
+
 sp.check_output('python3 train_imagenet_data_parallel.py \
 train.txt val.txt -a resnet50 -B 64 -j 8 -m train_mean.npy -R ~/workspace/ -o 1205 -E 1')
+
