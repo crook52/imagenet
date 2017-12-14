2088976692d50e83367f021ce084aede67f8dbf9
crook52 <kazuking52@gmail.com>
2017-12-14 20:38:53 +0900

    add runLR

--- 1/commander.py
+++ 1/commander.py
@@ -3,20 +3,67 @@
 
 import subprocess as sp
 import argparse
+import os
+import json
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--rootdir','--r',default='~/chainer-master')
-parser.add_argument('--o',default='result')
-parser.add_argument('--cuDNN',default=False)
-
+parser.add_argument('--rootdir', '--r', default='/home/cs28/PycharmProjects/ymd/chainer-master2')
+parser.add_argument('--o', default='test1')
+parser.add_argument('--cuDNN', default=False)
+parser.add_argument('--maxEpoch', default=3 ,type=int)
 args = parser.parse_args()
 
 if args.cuDNN:
-    sp.check_output("export CHAINER_CUDNN=1")
+    sp.check_output('export CHAINER_CUDNN=1',shell=True)
 else:
-    sp.check_output("export CHAINER_CUDNN=0")
+    sp.check_output('export CHAINER_CUDNN=0',shell=True)
 
+workspace = args.rootdir + '/examples/imagenet'
 
-sp.check_output('python3 train_imagenet_data_parallel.py \
-train.txt val.txt -a resnet50 -B 64 -j 8 -m train_mean.npy -R ~/workspace/ -o 1205 -E 1')
+os.chdir(workspace)
 
+print(os.getcwd())
+
+lr_list = [1.0, 0.5, 1.1]
+print(os.getcwd())
+sp.check_output('pwd',shell=True)
+
+main_output = args.o
+sub_output = args.o
+
+runLR = 0.01 ##init
+
+for epoch in range(args.maxEpoch):
+    best_err = float('inf')
+    bestLR = 1000000
+    for lr_num in range(len(lr_list)):
+
+        sub_cmd = 'python3 ymd_sub.py' \
+                  ' --out ' + sub_output + \
+                  ' --LR ' + str(lr_list[lr_num]*runLR) + \
+                  ' --epoch ' + str(epoch) + \
+                  ' --iteration ' + str(2)
+        print(sub_cmd)
+        sp.run(sub_cmd,shell=True)
+        filename = workspace+'/'+args.o+'/'+str(epoch)+'_'+str(lr_list[lr_num])
+        print(filename)
+        data = json.load(open(filename))
+        new_err = data[len(data)-1]['main/loss']
+        if best_err > new_err:
+            bestLR=lr_list[lr_num]
+            best_err = new_err
+
+    print('I chose {}',bestLR)
+    main_cmd = 'python3 ymd_main.py' \
+               ' --out ' + main_output +\
+               ' --LR ' + str(bestLR*runLR)+ \
+               ' --epoch ' + str(epoch)
+    print(main_cmd)
+    sp.run(main_cmd,shell=True)
+    runLR = bestLR*runLR
+
+#sp.check_output('cd'+workspace)
+#sp.check_output('python3 train_imagenet_data_parallel.py \
+#train.txt val.txt -a resnet50 -B 64 -j 8 -m train_mean.npy -R ~/workspace/ -o 1205 -E 1')
+
+#TODO [LR stoptriger model]を引数とした実行ファイルを作成して回す 生成物＝[err LR] 本体の実行ファイルに食わして、実行
---
+++ chainer-master2/examples/imagenet/ymd_main.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+"""Example code of learning a large scale convnet from LSVRC2012 dataset
+with multiple GPUs using data parallelism.
+
+Prerequisite: To run this example, crop the center of ILSVRC2012 training and
+validation images, scale them to 256x256 and convert them to RGB, and make
+two lists of space-separated CSV whose first column is full path to image and
+second column is zero-origin label (this format is same as that used by Caffe's
+ImageDataLayer).
+
+You need to install chainer with NCCL to run this example.
+Please see https://github.com/nvidia/nccl#build--run .
+
+"""
+from __future__ import print_function
+import argparse
+
+import numpy as np
+
+import chainer
+from chainer import training
+from chainer.training import extensions
+from chainer.training import updaters
+
+import alex
+import googlenet
+import googlenetbn
+import nin
+import resnet50
+import train_imagenet
+import csv
+import re
+def set_random_seed(seed):
+    # set Python random seed
+   # random.seed(seed)
+
+    # set NumPy random seed
+    np.random.seed(seed)
+
+    # set Chainer(CuPy) random seed
+   # cp.random.seed(seed)
+
+def main():
+    archs = {
+        'alex': alex.Alex,
+        'alex_fp16': alex.AlexFp16,
+        'googlenet': googlenet.GoogLeNet,
+        'googlenetbn': googlenetbn.GoogLeNetBN,
+        'googlenetbn_fp16': googlenetbn.GoogLeNetBNFp16,
+        'nin': nin.NIN,
+        'resnet50': resnet50.ResNet50
+    }
+
+    set_random_seed(0) ####固定
+
+    parser = argparse.ArgumentParser(
+        description='Learning convnet from ILSVRC2012 dataset')
+    parser.add_argument('--train', default='train.txt', help='Path to training image-label list file')
+    parser.add_argument('--val', default='val.txt', help='Path to validation image-label list file')
+    parser.add_argument('--arch', '-a', choices=archs.keys(),
+                        default='resnet50', help='Convnet architecture')
+    parser.add_argument('--batchsize', '-B', type=int, default=64,
+                        help='Learning minibatch size')
+    parser.add_argument('--epoch', '-E', type=int, default=1,
+                        help='Number of epochs to train')
+    parser.add_argument('--gpus', '-g', type=int, nargs="*",
+                        default=[0, 1])
+    parser.add_argument('--initmodel',
+                        help='Initialize the model from given file')
+    parser.add_argument('--loaderjob', '-j', type=int, default=8,
+                        help='Number of parallel data loading processes')
+    parser.add_argument('--mean', '-m', default='train_mean.npy',
+                        help='Mean file (computed by compute_mean.py)')
+    parser.add_argument('--resume', '-r', default='ymd_trainer',
+                        help='Initialize the trainer from given file')
+    parser.add_argument('--out', '-o', default='result',
+                        help='Output directory')
+    parser.add_argument('--root', '-R', default='/home/cs28/workspace/',
+                        help='Root directory path of image files')
+    parser.add_argument('--val_batchsize', '-b', type=int, default=250,
+                        help='Validation minibatch size')
+    parser.add_argument('--test', action='store_true')
+    parser.add_argument('--LR',default=0.01, type=float)
+    parser.set_defaults(test=False)
+    args = parser.parse_args()
+    best_lr = args.LR
+    resume_trainer = args.out + '/' + args.resume
+
+    # Initialize the model to train
+    model = archs[args.arch]()
+    if args.initmodel:
+        print('Load model from', args.initmodel)
+        chainer.serializers.load_npz(args.initmodel, model)
+
+    # Load the datasets and mean file
+    mean = np.load(args.mean)
+    train = train_imagenet.PreprocessedDataset(
+        args.train, args.root, mean, model.insize) ##Falseを追加でも固定できる
+    val = train_imagenet.PreprocessedDataset(
+        args.val, args.root, mean, model.insize, False)
+    # These iterators load the images with subprocesses running in parallel to
+    # the training/validation.
+    devices = tuple(args.gpus)
+
+    train_iters = [
+        chainer.iterators.MultiprocessIterator(i,
+                                               args.batchsize,
+                                               n_processes=args.loaderjob)
+        for i in chainer.datasets.split_dataset_n_random(train, len(devices),seed=1)]
+    val_iter = chainer.iterators.MultiprocessIterator(
+        val, args.val_batchsize, repeat=False, n_processes=args.loaderjob)
+
+    # Set up an optimizer
+    optimizer = chainer.optimizers.MomentumSGD(lr=args.LR, momentum=0.9)
+    optimizer.setup(model)
+
+    # Set up a trainer
+    updater = updaters.MultiprocessParallelUpdater(train_iters, optimizer,
+                                                   devices=devices)
+    #trainer = training.Trainer(updater, (args.epoch, 'epoch'), args.out)
+    trainer = training.Trainer(updater, (args.epoch + 1, 'epoch'), args.out)
+    if args.test:
+        val_interval = 5, 'epoch'
+        log_interval = 1, 'epoch'
+    else:
+        val_interval = 100000, 'iteration'
+        log_interval = 100, 'iteration'
+        snapshot_interval = 1, 'epoch'
+
+    trainer.extend(extensions.Evaluator(val_iter, model, device=args.gpus[0]),
+                   trigger=val_interval)
+    trainer.extend(extensions.dump_graph('main/loss'))
+    trainer.extend(extensions.snapshot(filename='ymd_trainer'), trigger=snapshot_interval)
+    # trainer.extend(extensions.snapshot_object(
+    #     model, 'model_iter_{.updater.iteration}'), trigger=snapshot_interval)
+    trainer.extend(extensions.snapshot_object(
+        model, 'ymd_model'), trigger=snapshot_interval)
+    # Be careful to pass the interval directly to LogReport
+    # (it determines when to emit log rather than when to read observations)
+    trainer.extend(extensions.LogReport(trigger=log_interval, log_name='main_log'))
+    trainer.extend(extensions.observe_lr(), trigger=log_interval)
+    trainer.extend(extensions.PrintReport([
+        'epoch', 'iteration', 'main/loss', 'validation/main/loss',
+        'main/accuracy', 'validation/main/accuracy', 'lr'
+    ]), trigger=log_interval)
+    trainer.extend(extensions.ProgressBar(update_interval=2))
+
+    if not args.epoch == 0:
+        if args.resume:
+            chainer.serializers.load_npz(resume_trainer, trainer)
+            trainer.updater.get_optimizer('main').lr = best_lr
+    trainer.run()
+    # print(trainer.observation)
+    # loss = trainer.observation['main/loss']
+    # pattern=r'([0-9]+.[0-9]+)'
+    # loss=re.findall(pattern,str(loss))
+    # filename = args.out+'/loss.csv'
+    # f = open(filename,'a',newline='')
+    # writer = csv.writer(f, lineterminator='\n')
+    # writer.writerow(loss)
+    # f.close()
+
+if __name__ == '__main__':
+    main()
